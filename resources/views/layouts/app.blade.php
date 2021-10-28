724301e8df4487aada7860e94943c9143dd7ccec
Stefano009 <stefano.funaro@gmail.com>
2021-10-28 10:12:16 +0200

    linking css with asset is better because it is more secure and make some independents controls

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    {{-- ora non mi cambia nulla usare o no asset per richiamare il css perchè ci fa dei controlli ulteriori che salva tempo e da più sicurezza --}}
     <title>@yield('title') | DC comix</title>
 </head>
 <body>  
